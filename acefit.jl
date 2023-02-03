8fe51207b8980727fb1393cf881f6fe9eb2a10ba
humashankar <71093689+hari-ushankar@users.noreply.github.com>
2023-02-02 19:25:16 -0800

    push more changes

--- acefit.jl
+++ acefit.jl
@@ -4,7 +4,7 @@ data = JuLIP.read_extxyz("master.xyz")
 r0 = 2.55
 ACE_B = ace_basis(species = [:Cu],
                   N = 3,
-                  maxdeg = 12,
+                  maxdeg =6,
                   rcut = 5.506786,
                   r0 = r0,
                   rin = 0.6 * r0, pin = 2);
@@ -15,10 +15,11 @@ Bpair = pair_basis(species = [:Cu],
 B = JuLIP.MLIPs.IPSuperBasis([Bpair, ACE_B]);
 Vref = OneBody(:Cu => -3.54)
 weights = Dict(
-        "bulk" => Dict("E" => 20.0, "F" => 5.0 , "V" => 3.0 ),
+        "bulk" => Dict("E" => 1.0, "F" => 1.0 , "V" => 1.0 ),
         );
 ##
-train = [ACE1pack.AtomsData(t,"energy","forces","virial",weights,Vref) for t in data]
+train = [ACE1pack.AtomsData(t,"energy","forces","stress",weights,Vref) for t in data]
+train_data = train[275:end]
 A, Y, W = ACEfit.linear_assemble(train, B);
 ##
 solver_type = :lsqr
